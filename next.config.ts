1ffb276f1ffad154a6c7e95481e6f65d3bd87407
moeez7978-linux <moeez7978@outlook.com>
2026-07-22 17:59:55 +0500

    Added gh-pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
+const isPagesDeployment = process.env.NEXT_PUBLIC_BASE_PATH === "/Portfolio";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
+  ...(isPagesDeployment && {
+    basePath: "/Portfolio",
+    assetPrefix: "/Portfolio",
+  }),
 };
 
 export default nextConfig;
